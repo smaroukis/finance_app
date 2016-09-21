7771b53ff5e951859c1a6727631c4c7046e4c7e0
Spencer Maroukis <smarouki@umich.edu>
2016-09-21 19:46:31 -0400

    new commands

--- finance_app.py
+++ finance_app.py
@@ -23,13 +23,16 @@ transport = ['Public Transportation','Gas & Fuel', 'Rental Car & Taxi','Auto & T
 travel = ['Travel','Coach Bus','Air Travel','Hotel']
 drinks = ['Alcohol & Bars']
 sport = ['Sports','Sporting Goods','Hobbies']
-shopping = ['Electronics & Software','Music','Books','Clothing','Books & Supplies','Gift','Office Supplies','Home Improvement','Mobile Phone'] # should it be a set?
+shopping = ['Electronics & Software','Music','Books','Clothing','Books & Supplies','Gift','Office Supplies','Mobile Phone'] # should it be a set?
 entertainment = ['Event Tickets','Entertainment','Amusement','Arts']
 fees = ['ATM Fee','Finance Charge','Fees & Charges','Bank Fee','State Tax','Federal Tax','Service Fee']
 misc = ['Printing','Hair','Uncategorized','Business Services','Transfer for Cash Spending','Doctor','Eyecare','Misc Expenses']
-home = ['Home','Utilities','Mortgage & Rent','Rent',]
+home = ['Home','Utilities','Mortgage & Rent','Rent', 'Home Improvement']
 edu = ['Education','Tuition']
 hidden = ['Hide from Budgets & Trends']
 allcats = food+coffee+transport+travel+drinks+sport + shopping+ entertainment+fees+misc+home+edu+hidden
 # More ideas: Recurring, Software, Big Spends
 # Not included: TODO wrote this somewhree
+
+# df.loc[df['col'].isin(list)] || df.['col'].isin(list).all(0)
+df['start date':'end date']
