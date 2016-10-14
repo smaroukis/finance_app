b96867445de9d67f78352cd35bff4ed6b936b721
Spencer Maroukis <smarouki@umich.edu>
2016-10-14 00:15:55 -0400

    set difference

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Configuration file, updated 9/2/16
 
-dirs = {'dl': r"~/Downloads/transactions.csv", 'out':'~/Box/finance_app.xlsx'}
+dirs = {'dl': r"transactions.csv", 'out':'finance_app.xlsx'}
--- finance_app.py
+++ finance_app.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import itertools
+import numpy as np
 import os
 import datetime
 from config import *
@@ -7,16 +9,13 @@ from config import *
 dl_dir = dirs['dl']
 out_dir = dirs['out']
 
-
 # Todo: Autodownload
 
 # Read Data in, index as datetime indices
 df = pd.read_csv(dl_dir, parse_dates = 'Date', index_col = 'Date')
-
-df = df.truncate(after = '07/01/2016')
+#df = df.truncate(after = '07/01/2016')
 
 # Defining Groups
-
 food = ['Restaurants','Fast Food','Groceries','Food & Dining']
 coffee = ['Coffee Shops']
 transport = ['Public Transportation','Gas & Fuel', 'Rental Car & Taxi','Auto & Transport','Parking']
@@ -29,10 +28,18 @@ fees = ['ATM Fee','Finance Charge','Fees & Charges','Bank Fee','State Tax','Fede
 misc = ['Printing','Hair','Uncategorized','Business Services','Transfer for Cash Spending','Doctor','Eyecare','Misc Expenses']
 home = ['Home','Utilities','Mortgage & Rent','Rent', 'Home Improvement']
 edu = ['Education','Tuition']
+llexpenses = [food, coffee, transport, travel, drinks, sport, shopping, entertainment, fees, misc, home, edu]
+llnames = ['food','coffee','transport','travel','drinks','sport','shopping','entertainment','fees','misc', 'home', 'edu']
 hidden = ['Hide from Budgets & Trends']
-allcats = food+coffee+transport+travel+drinks+sport + shopping+ entertainment+fees+misc+home+edu+hidden
 # More ideas: Recurring, Software, Big Spends
-# Not included: TODO wrote this somewhree
+
+
+# Expenses that aren't accounted for
+expenses_user = list(itertools.chain(*llexpenses)) # use itertools module to iterate over flattened list
+expenses = df.Category.unique().tolist() # all categories
+set(expenses_user) ^ set(expenses) # now write a test for this
+
+
 
 # df.loc[df['col'].isin(list)] || df.['col'].isin(list).all(0)
-df['start date':'end date']
+#df['start date':'end date']
