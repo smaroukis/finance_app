d53cfbfa6fe83c35100c7b4f48187e1e47225d96
Spencer Maroukis <smarouki@umich.edu>
2017-02-12 15:22:37 -0500

    rm config

--- config.py
+++
@@ -1,6 +0,0 @@
-# Configuration file, updated 9/2/16
-
-dirs = {'transactions': r"transactions.csv", 'outfile':'finance_app.xlsx'}
-
-# for plaid
-pld = dict(institutions = 'https://tartan.plaid.com/institutions', client_id = '58991dd24e95b81dc56186c9', public_key = 'db10d55d87be474bb2087300c29653', secret = 'bdfef2d11864cf0b3f2f0e3aca16f5')
