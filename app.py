c638062fc730c6218e6fd378ed0a89e3d5e8f208
Spencer Maroukis <smarouki@umich.edu>
2017-03-17 20:12:42 -0500

    Created twilio client and creds

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import pyrebase
 from pyrebase_config import update_db, get_transactions
+from config import twil_config
+from twilio.rest import TwilioRestClient
 
+client = TwilioRestClient(twil_config['test_account_sid'], twil_config['test_auth_token'])
+
+# message=client.messages.create(to=to_number, from=from_number, body=message)
+body="Hello World"
+message=client.messages.create(to=twil_config['to_number'], from_=twil_config['from_number'], body=body)
 # db updated i pyrebase_confi
 # user="test"
 
@@ -22,8 +29,16 @@ def filter_date_range(user, start, end):
     data=[i for i in trans if i['date']<=end if i['date']>=start]
     return data
 
-def get_categories():
-    pass #TODO return unique categories
+def get_categories(user, query=None):
+    """Get all the categories from a certain query"""
+    if query is None:
+        # Get all
+        trans=get_transactions(user)
+    else
+        trans=query
+    return [i['category'] for i in  trans]
 
 def filter_categories():
     pass
+
+### TWILIO functions
