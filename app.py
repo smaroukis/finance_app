73869497dd0e1e831ab3e12d05318014a93d6b28
smarouki <smarouki@umich.edu>
2017-03-25 22:28:20 -0400

    working on query routing

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import pyrebase
 from pyrebase_config import update_db, get_transactions, get_balance
-from config import twil_config
+from config import twil_config, secret_key
 from twilio.rest import TwilioRestClient
 from twilio import twiml
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, session
 
 ### Flask
 application = Flask(__name__)
-
 ### Twilio: create client (sandbox/trial)
 client = TwilioRestClient(twil_config['account_sid'], twil_config['auth_token'])
 # message=client.messages.create(to=to_number, from=from_number, body=message)
@@ -16,24 +15,35 @@ client = TwilioRestClient(twil_config['account_sid'], twil_config['auth_token'])
 
 @application.route('/')
 def root():
-    # TODO user session cookies
     response = twiml.Response()
 
-    if 'hopslam' is in session:
+
 
     try:
         body = request.form['Body']
         print(body)
     except:
         print("couldn't find message body")
-        messages = client.messages.list()
+        messages = client.messages.list() # TODO ask Al what this does
         body = messages[0].body
 
     request = body.lower().strip() # TODO comes from Twilio? getting error
+
+    print('session is: \n')
+    print(session)
+    # Check to see if user has launched a query
+    if 'query' in session:
+        try:
+            response.redirect('query') # to direct to query/<arg> use ..arg=value
+        except:
+            print("Error on redirect, removing session key")
+            session.pop('query', None)
+        return str(response)
+
     if request in ["balance", "bal", "b"]:
         message = return_balance('user1')
     elif request in ["query", "q"]:
-        message = launch_query('user1')
+        message = launch_query()
     else:
         message = "Usage: \n (1) 'balance', 'bal', or 'b' for "\
             "account balance. \n (2) 'query', 'q' for launching a query.\n"
@@ -48,54 +58,20 @@ def return_balance(user):
         message += " {}: {} \n".format(k, v)
     return message
 
-def launch_query(user):
-    response = twiml.Response()
-    # TODO create session cookie to put in root route for query
-    response.redirect(url_for('query'))
-    session['hopslam'] = True
-    return str(response)
+def launch_query():
+    session['query'] = 'launched'
+    print(session)
+    message = "You just launched a query. Current options:\n"\
+            "-b  get transactions before date MM/DD/YYYY\n"\
+            "-a  get transactions after date MM/DD/YYYY\n"\
+            "-o  get all transactions over $X\n"\
+            "-u  get all transactions under $X\n"
+    return message # TODO can you redirect and return a message?
 
     # Redirect to /query which will prompt
     #response.redirect('/query', method='GET')
-
+application.secret_key=secret_key
     #return str(response)
-
-# use session key here somewhere
-# use url redirect
-
-#def bank_query():
-
-### Filter Functions
-
-def filter_amt_over(user, amount):
-    # assume the account has been instantiated
-    # user here is the firebase db child, let's make it match the plaid username
-    trans=get_transactions(user)
-    data=[i for i in trans if i['amount']>amount]
-    return data
-
-def filter_amt_under(user, amount):
-    trans=get_transactions(user)
-    data=[i for i in trans if i['amount']<amount]
-    return data
-
-def filter_date_range(user, before, after):
-    # Returns inclusive list. Inputs should be YYYY-MM-DD string
-    trans=get_transactions(user)
-    data=[i for i in trans if i['date']<=before if i['date']>=after]
-    return data
-
-def get_categories(user, query=None):
-    """Get all the categories from a certain query"""
-    if query is None:
-        # Get all
-        trans=get_transactions(user)
-    else:
-        trans=query
-    return [i['category'] for i in  trans]
-
-def filter_categories():
-    pass
 
 if __name__=="__main__":
     application.run(debug=True)
